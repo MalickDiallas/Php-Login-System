e9b33683d43c34c9d53f40f4d46088575dbb05df
Malick Diallo <malick.diallo9891@gmail.com>
2020-02-18 00:55:32 +0000

    add fichier ukit

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
-<?php echo"Hello word";?>
+<?php echo"Hello word ";?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="utf-8">
+	<title></title>
+	<link rel="stylesheet" type="text/css" href="css/uikit.min.css">
+</head>
+<body>
+<!--Jquery-->
+<script  src="js/Jquery-3.4.1.js"></script>
+<!--Uikit js-->
+<script  src="js/uikit.min.js"></script>
+<!-- script js-->
+<script src="js/script.js"></script>
+</body>
+</html>
