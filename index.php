bd69467c6fcd56db921a94a1d9142d7312c53cec
Malick Diallo <malick.diallo9891@gmail.com>
2020-02-18 01:17:12 +0000

    Add login form

--- index.php
+++ index.php
@@ -1,12 +1,39 @@
-<?php echo"Hello word ";?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
 	<meta charset="utf-8">
 	<title></title>
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	 <meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" type="text/css" href="css/uikit.min.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<meta name="robots" content="follow">
 </head>
 <body>
+
+<div class="uk-section uk-container">
+	<div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid>
+		<form class="uk-form-stacked js-login">
+
+			    <div class="uk-margin">
+			        <label class="uk-form-label" for="form-stacked-text">Email</label>
+			        <div class="uk-form-controls">
+			            <input class="uk-input" id="form-stacked-text" type="email" placeholder="Email@email.com" required="required">
+			        </div>
+			    </div>
+			    <div class="uk-margin">
+			        <label class="uk-form-label" for="form-stacked-text">Password</label>
+			        <div class="uk-form-controls">
+			            <input class="uk-input" id="form-stacked-text" type="password" placeholder="Your password" required="required">
+			        </div>
+			    </div>
+			    <div class="uk-margin">
+			        <button class="uk-button uk-button-default" type="submit">Login</button>
+			    </div>
+
+		</form>
+	</div>
+</div>
 <!--Jquery-->
 <script  src="js/Jquery-3.4.1.js"></script>
 <!--Uikit js-->
